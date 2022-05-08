83522b023de9049270378aa432622f6e54fa4bf2
Cristian Morataya <cris.morataya@gmail.com>
2022-05-08 16:59:34 -0600

    Add ui styles

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:dsc_flutter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:dsc_flutter/ui/input_decorations.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -24,11 +27,75 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(
                     height: 30,
                   ),
-                  Text('Formulario')
+                  _LoginForm()
                 ]),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Crear nueva cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 50,
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: mantener referencia al key
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'jhone.doe@gmail.com',
+                  labelText: 'Correo electrónico',
+                  prefixIcon: Icons.alternate_email_sharp),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '******',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(
+                    'Ingresar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                onPressed: () {
+                  // TODO: submit to login
+                }),
+          ],
         ),
       ),
     );
